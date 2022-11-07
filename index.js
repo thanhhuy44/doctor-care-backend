e2282899933a4d03961e88b245541894c3bc52fa
thanhhuy44 <thhuy444111@gmail.com>
2022-11-07 13:35:06 +0700

    update

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.raw());
 app.use(fileUpload());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(3030, () => {
   console.log("App listening on port 3030");
